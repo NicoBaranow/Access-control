0e41a92be8cd61ad8221e7e1915f3b66ea0e36cb
NicoBaranow <70355656+NicoBaranow@users.noreply.github.com>
2022-05-25 15:44:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
+from tkinter.tix import InputOnly
 import NFC_PN532 as nfc
 import PN532
 from machine import Pin
 import time
 import utime 
+from timeit import default_timer as timer
 
 led = Pin(2,Pin.OUT)
 iman = Pin(15,Pin.OUT)
 but = Pin(4,Pin.OUT)
-profesores = ["abc","abcc","aaccas"]
+profesores = []
 
 iman.value(0)
 intTime = 0
 debTime = 100
-
 
 #Interrupcion apertura desde dentro
 def abrir(Pin):
@@ -32,8 +33,38 @@ but.irq(handler=abrir, trigger=Pin.IRQ_FALLING)
 #PN532 initiation
 PN532.init()
 
-print("Waiting for RFID/NFC card...")
+print("Esperando un tag RFID/NFC...")
 
-uid = PN532.access() 
+while True:
+    uid = ""
+    imp = ""
+    uid = PN532.access() 
+    inp = input()
 
-if uid in 
+    if uid in profesores:
+        start = timer()
+        iman.value(1)
+        #time.sleep(300)
+        #iman.value(0)
+        if start >= 3000000: #supongo que siempre quedara abierto
+            iman.value(0)
+    else:
+        print("El tag no está registrado")
+
+    if inp == "1":
+        print("Apoye un tag para agregar a los tags autorizados")
+        if uid != "": #no va a llegar a leer el tag por lo que va a aparecer como que nunca se apoya, tal vez la solucion es no inicializarlo en "" al principio,
+                      #lo mismo para el inp
+            profesores.append(uid)
+            print("Accion completada con exito! los tags autorizados son: " + profesores)
+        else:
+            print("Error! No se ha encontrado un tag")
+        
+    if inp == "0":
+        print("Apoye un tag para remover de los tags autorizados")
+        if uid != "": #no va a llegar a leer el tag por lo que va a aparecer como que nunca se apoya, tal vez la solucion es no inicializarlo en "" al principio,
+                      #lo mismo para el inp
+            profesores.remove(uid)
+            print("Accion completada con exito! los tags autorizados son: " + profesores)
+        else:
+            print("Error! No se ha encontrado un tag")
