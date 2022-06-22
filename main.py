0b982995bae40d8e1c5ddc98e8da679fa64555b3
NicoBaranow <70355656+NicoBaranow@users.noreply.github.com>
2022-06-22 11:42:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,79 +1,66 @@
 # import NFC_PN532 as nfc
 import PN532
-from machine import Pin
+from machine import Pin, PWM, Timer
 import time
-import utime 
 # from timeit import default_timer as timer
 
-led = Pin(2,Pin.OUT)
+led = Pin(19,Pin.OUT)
+led1 = Pin(18,Pin.OUT)
 iman = Pin(12,Pin.OUT)
 but = Pin(33,Pin.IN, Pin.PULL_DOWN)
-profesores = []
+speaker = PWM(Pin(5))
+# tim = Timer()
+# tim.init(period = 1000, mode=Timer.PERIODIC, callback = temporizador)
+
+sentido = True
+contador = 0
+
+led.value(0)
+led.value(0)
+speaker.freq(1)
+speaker.duty(0)
+iman.value(0)
 
 intTime = 0
 debTime = 100
 
-iman.value(0)
+profesores = ["51:75:24:d9", "b6:71:61:5e", "82:f5:97:4", "4:5a:10:b2:73:57:80", "c2:8a:dc:29", "4:8c:71:82:73:57:80", "a2:89:69:dd", "cf:5e:4e:38", "2f:5:26:4", "af:f3:33:59", "c3:7c:44:19", "4:73:1f:52:4b:5f:80", "b3:3b:99:14", "4:8a:26:52:4e:5f:80", "ef:e5:11:34", "4:69:c:2:7c:56:80"]
 
-#PN532 initiation
-PN532.init(),
-def abrir(Pin):
-    global intTime
-    if utime.ticks_diff(utime.ticks_ms(), intTime) > debTime:
+#apertura
+def puerta():
         led.value(1)
+        led1.value(1)
         iman.value(1)
-        print("Funcion abrir")
-        time.sleep(3)
-        print("Funcion abrir terminada")
+        speaker.freq(2000)
+        speaker.duty(900)
+        time.sleep(2)
+        speaker.freq(1)
+        speaker.duty(0)
         led.value(0)
+        led1.value(0)
         iman.value(0)
-        intTime = utime.ticks_ms()
 
-but.irq(handler=abrir, trigger=Pin.IRQ_FALLING)
+#Handler interrupcion
+def abrir(Pin):
+    print("6")
+    global intTime
+    if time.ticks_diff(time.ticks_ms(), intTime) > debTime and but.value() == 1:
+        print("interrupcion ejecutada")
+        puerta()
+        intTime = time.ticks_ms()
+
+#interrupcion boton
+but.irq(handler=abrir, trigger=Pin.IRQ_RISING)
+
+#PN532 iniciacion
+PN532.init()
 
 print("Esperando un tag RFID/NFC...")
+
 while True:
     uid = ""
-    uid = PN532.access() 
-    if uid =="cf:5e:4e:38" or uid=="af:f3:33:59" or uid=="c3:7c:44:19" or uid=="4:73:1f:52:4b:5f:80" or "b3:3b:99:14" or "4:8a:26:52:4e:5f:80" or "ef:e5:11:34":
-        iman.value(1)
-        time.sleep(3)
-        iman.value(0)
-        
-# while True:
-#     uid = ""
-#     imp = ""
-#     uid = PN532.access() 
-#     inp = input()
-# 
-#     while inp == "":
-#                 
-#         if uid in profesores:
-#             start = timer()
-#             
-#             while start <= 3000000:
-#                 iman.value(1)
-#                 
-#             iman.value(0)
-#             
-#         else:
-#             print("El tag no está registrado")
-#             
-#     while inp == "1":
-#         print("Apoye un tag para agregar a los tags autorizados")
-#         if uid != "": #no va a llegar a leer el tag por lo que va a aparecer como que nunca se apoya, tal vez la solucion es no inicializarlo en "" al principio,
-#                       #lo mismo para el inp
-#             profesores.append(uid)
-#             print("Accion completada con exito! los tags autorizados son: " + profesores)
-#         else:
-#             print("Error! No se ha encontrado un tag")
-#             
-#     while inp == "0":
-#         print("Apoye un tag para remover de los tags autorizados")
-#         if uid != "": #no va a llegar a leer el tag por lo que va a aparecer como que nunca se apoya, tal vez la solucion es no inicializarlo en "" al principio,
-#                       #lo mismo para el inp
-#             profesores.remove(uid)
-#             print("Accion completada con exito! los tags autorizados son: " + profesores)
-#         else:
-#             print("Error! No se ha encontrado un tag")
+    uid = PN532.access()
+    if uid in profesores:
+        print("tarjeta habilitada")
+        puerta()
 
